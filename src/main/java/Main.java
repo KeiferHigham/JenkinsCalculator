1bfcb6fabea91376b20c5ca16469cd7556166926
Keifer <keifer.higham22@gmail.com>
2021-10-22 18:17:23 -0600

    fix command line functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,27 +6,28 @@ public class Main {
     System.out.println("Welcome To the Calculator");
     int second = 0;
     String functionToRun = args[0];
-    int firstNum = Integer.getInteger(args[1]);
+    String firstNum = args[1];
+    int firstNumber = Integer.valueOf(firstNum);
     if(!functionToRun.equals(null)) {
-      second = Integer.getInteger(args[2]);
+      second = Integer.valueOf(args[2]);
     }
 
     Calculator calc = new Calculator();
 
     if (args[0].equals("add")) {
-      System.out.println(calc.add(firstNum,second));
+      System.out.println(calc.add(firstNumber,second));
     }
     else if(args[0].equals("subtract")) {
-      System.out.println(calc.subtract(firstNum,second));
+      System.out.println(calc.subtract(firstNumber,second));
     }
     else if(args[0].equals("multiply")) {
-      System.out.println(calc.multiply(firstNum,second));
+      System.out.println(calc.multiply(firstNumber,second));
     }
     else if(args[0].equals("binary")) {
-      System.out.println(calc.intToBinaryNumber(firstNum));
+      System.out.println(calc.intToBinaryNumber(firstNumber));
     }
     else if(args[0].equals("fibonacci")) {
-      System.out.println(calc.fibonacciNumberFinder(firstNum));
+      System.out.println(calc.fibonacciNumberFinder(firstNumber));
     }
     else {
       System.out.println("Please type in valid input");
